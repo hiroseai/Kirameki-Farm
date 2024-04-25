d1469333d7e87429f6edf068b11f9bb840632c7a
ai.hirose <h7tm2a@outlook.jp>
2024-04-25 17:55:44 +0900

    4/25最新

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,10 +1,38 @@
 /* header
 ------------------------------------------ */
-$('.header__hamburger-button').on('click', function (e) {
-    $(".header__nav").slideToggle();
+var hamburger = $('.header__nav');
+
+$('.header__hamburger-button').on('click', function () {
+    hamburger.toggleClass('header__nav-active');
+});
+
+$(window).on('resize', function () {
+    hamburger.removeClass('header__nav-active');
 });
 
 $('.header__nav a[href^="#"]').on('click', function () {
     $('.header__hamburger-button').click();
 });
 
+/* cover
+------------------------------------------ */
+var swiper = new Swiper('.swiper', {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: "clickable"
+    },
+});
+
+/* faq
+------------------------------------------ */
+$('.faq__item-question').on('click', function (e) {
+    $(e.target).toggleClass('faq__item-question-active');
+
+    var content = $(e.target).next();
+    content.slideToggle();
+});
